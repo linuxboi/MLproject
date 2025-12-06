c40bd134e7900b7cdd417ad7a725af1ed3e30a18
othmane <othmane.tachafine@gmail.com>
2025-12-06 19:02:57 +0100

    model training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
 @dataclass
 class DataIngestionConfig:
     def __init__(self):
@@ -47,4 +50,8 @@ if __name__=="__main__":
     train_data_path, test_data_path = obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+    train_array,test_array,preprocessor_obj=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+    model_trainer=ModelTrainer()
+    r2_square=model_trainer.initiate_model_trainer(train_array,test_array,preprocessor_obj)
+    print("R2 square value :",r2_square)
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 
 import dill
+from sklearn.metrics import r2_score
 
 from src.exception import CustomException
 
@@ -16,5 +17,21 @@ def save_object(file_path,obj):
         with open(file_path,'wb') as file_obj:
             dill.dump(obj,file_obj)
             
+    except Exception as e:
+        raise CustomException(e,sys)
+def evaluate_models(X_train,y_train,X_test,y_test,models):
+    try:
+        report={}
+
+        for i in range(len(models)):
+            model=list(models.values())[i]
+            model.fit(X_train,y_train)
+
+            y_test_pred=model.predict(X_test)
+
+            r2_square=r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]]=r2_square
+        return report
     except Exception as e:
         raise CustomException(e,sys)
