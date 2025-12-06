895763da1649c47d08a24da207b8dce6d6c6305e
othmane <othmane.tachafine@gmail.com>
2025-12-06 17:05:09 +0100

    data ingestion and transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,11 +1,14 @@
 import os 
 import sys  
-from exception import CustomException
-from logger import logging
+from src.exception import CustomException
+from src.logger import logging
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -38,4 +41,10 @@ class DataIngestion:
                 self.ingestion_config.test_data_path
             )
         except Exception as e:
-            raise CustomException(e,sys)            
+            raise CustomException(e,sys)
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data_path, test_data_path = obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data_path,test_data_path)
