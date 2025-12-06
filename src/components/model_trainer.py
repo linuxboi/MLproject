f3f32905b41bc4ddf2ebe846d768347f3255fbc1
othmane <othmane.tachafine@gmail.com>
2025-12-06 19:23:52 +0100

    param tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -47,15 +47,53 @@ class ModelTrainer:
                 "CatBoosting Regressor":CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor":AdaBoostRegressor()
             }
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                            models=models) 
-               
-            best_model_score=max(sorted(model_report.values())) 
-            best_model_name=list(model_report.keys())[
+            params={    
+                'Decision Tree':{
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+                'Random Forest':{
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                'Gradient Boosting':{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                'Linear Regression':{},
+                'K-Neighbours':{
+                    'n_neighbors':[3,5,7,9,11],
+                    'weights':['uniform','distance'],
+                    'algorithm':['auto','ball_tree','kd_tree']
+                },
+                'XGBRegressor':{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                'CatBoosting Regressor':{
+                    'depth':[6,8,10],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'iterations':[30,50,100]
+                },
+                'AdaBoost Regressor':{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                }
+            }
+            model_report, tuned_models = evaluate_models(
+                X_train=X_train,
+                y_train=y_train,
+                X_test=X_test,
+                y_test=y_test,
+                models=models,
+                param=params,
+            )
+
+            best_model_score = max(sorted(model_report.values()))
+            best_model_name = list(model_report.keys())[ 
                 list(model_report.values()).index(best_model_score)
             ]
 
-            best_model=models[best_model_name]
+            best_model = tuned_models[best_model_name]
 
             if best_model_score<0.6:
                 raise CustomException("No best model found")
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import numpy as np
 
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV as gridSearchCV
 
 from src.exception import CustomException
 
@@ -14,24 +15,28 @@ def save_object(file_path,obj):
         dir_path=os.path.dirname(file_path)
         os.makedirs(dir_path,exist_ok=True)
 
-        with open(file_path,'wb') as file_obj:
+        with open(file_path,'wb') as file_obj: 
             dill.dump(obj,file_obj)
             
     except Exception as e:
         raise CustomException(e,sys)
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     try:
         report={}
+        tuned_models={}
 
-        for i in range(len(models)):
-            model=list(models.values())[i]
-            model.fit(X_train,y_train)
+        for name, model in models.items():
+            para = param.get(name, {})
+            gs = gridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
 
-            y_test_pred=model.predict(X_test)
+            best_model = gs.best_estimator_  # already refit on full training when refit=True
+            y_test_pred = best_model.predict(X_test)
 
-            r2_square=r2_score(y_test,y_test_pred)
+            r2_square = r2_score(y_test, y_test_pred)
+            report[name] = r2_square
+            tuned_models[name] = best_model
 
-            report[list(models.keys())[i]]=r2_square
-        return report
+        return report, tuned_models
     except Exception as e:
         raise CustomException(e,sys)
